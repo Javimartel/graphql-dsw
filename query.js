b127f176660f3a6d536ae74413d6ae103dfb5e37
Javi <javiermespino@gmail.com>
2023-02-25 03:15:19 +0000

    Add Products test

--- query.js
+++ query.js
@@ -2,16 +2,23 @@ const { request } = require('graphql-request');
 
 const url = 'http://localhost:4000/graphql';
 
-const query = `
-  query {
-    getProducts {
-      id
-      name
-      price
-    }
-  }
-`;
+const addProduct = (name, description, price, stock) => {
+  const query = `
+    mutation {
+      addProduct(name: "${name}", description: "${description}", price: ${price}, stock: ${stock}) {
+        id
+        name
+        description
+        price
+        stock
+      }
+    }  
+  `;
+  
+  request(url, query).then(data => {
+    console.log(data);
+  });
+}
 
-request(url, query).then(data => {
-  console.log(data);
-});
+// Llamada a la función addProduct con los parámetros correspondientes
+addProduct('Test', 'Descripción test', 10, 100);
